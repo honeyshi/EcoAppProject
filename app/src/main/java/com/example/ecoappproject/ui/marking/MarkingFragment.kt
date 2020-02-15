70b3ff5dfb9d80f904a19db458203f2f924fd131
Катя Тихонова <missoctober390@gmail.com>
2020-02-15 21:51:30 +0300

    make UI for eco marking fragment

--- app/src/main/java/com/example/ecoappproject/ui/marking/MarkingFragment.kt
+++ app/src/main/java/com/example/ecoappproject/ui/marking/MarkingFragment.kt
@@ -1,12 +1,17 @@
 package com.example.ecoappproject.ui.marking
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.ecoappproject.R
 import com.example.ecoappproject.objects.EcoMarkingObject
+import com.example.ecoappproject.ui.home.HomeFragment
 
 class MarkingFragment : Fragment() {
     override fun onCreateView(
@@ -18,6 +23,19 @@ class MarkingFragment : Fragment() {
         EcoMarkingObject.clearEcoMarkingList()
         EcoMarkingObject.getEcoMarkings(activity!!.applicationContext,
             root.findViewById(R.id.recycler_view_eco_marking))
+
+        root.findViewById<ImageButton>(R.id.image_button_marking_fragment_left).setOnClickListener{
+            leftArrowClickListener()
+        }
         return root
+    }
+
+    private fun leftArrowClickListener(){
+        Log.w(ContentValues.TAG, "Start eco articles fragment")
+        val homeFragment = HomeFragment()
+        val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment, homeFragment)
+        //transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
