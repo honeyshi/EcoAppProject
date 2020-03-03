b021b1bf568e3e3dd87804b7c18fa0e3a07f9bca
Катя Тихонова <missoctober390@gmail.com>
2020-03-03 08:58:10 +0300

    add arrow button click listeners in Marking fragment

--- app/src/main/java/com/example/ecoappproject/ui/marking/MarkingFragment.kt
+++ app/src/main/java/com/example/ecoappproject/ui/marking/MarkingFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.ecoappproject.R
 import com.example.ecoappproject.objects.EcoMarkingObject
+import com.example.ecoappproject.ui.challenge.ChallengeFragment
 import com.example.ecoappproject.ui.home.HomeFragment
 
 class MarkingFragment : Fragment() {
@@ -27,15 +28,27 @@ class MarkingFragment : Fragment() {
         root.findViewById<ImageButton>(R.id.image_button_marking_fragment_left).setOnClickListener{
             leftArrowClickListener()
         }
+
+        root.findViewById<ImageButton>(R.id.image_button_marking_fragment_right).setOnClickListener {
+            rightArrowClickListener()
+        }
+
         return root
     }
 
     private fun leftArrowClickListener(){
-        Log.w(ContentValues.TAG, "Start eco articles fragment")
-        val homeFragment = HomeFragment()
+        Log.w("Eco Marking fragment", "Start eco articles fragment")
         val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
-        transaction.replace(R.id.nav_host_fragment, homeFragment)
-        //transaction.addToBackStack(null)
+        transaction.replace(R.id.nav_host_fragment, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private fun rightArrowClickListener(){
+        Log.w("Eco Marking fragment", "Start challenges fragment")
+        val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment, ChallengeFragment())
+        transaction.addToBackStack(null)
         transaction.commit()
     }
 }
