4ed00a2376fefbc1addbf2461635c2665bff7ad5
Катя Тихонова <missoctober390@gmail.com>
2020-04-01 20:55:58 +0300

    convert anonymous account via Google sign in

--- app/src/main/java/com/example/ecoappproject/LoginActivity.kt
+++ app/src/main/java/com/example/ecoappproject/LoginActivity.kt
@@ -16,9 +16,10 @@ import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var googleSignInClient : GoogleSignInClient
+    private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var firebaseAuth: FirebaseAuth
-    private val RC_SIGN_IN = 9001
+    private val RC_SIGN_IN = 9001     // request code for Google sign in
+    private val RC_CONVERT_ACC = 9002 // request code for convert Anonymous account to permanent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,23 +36,26 @@ class LoginActivity : AppCompatActivity() {
         // Initialize Firebase Auth
         firebaseAuth = FirebaseAuth.getInstance()
 
-        // Set listeners for buttons
-        button_login_activity_google.setOnClickListener{
-            signInWithGoogle()
-        }
-
-        // If user is not signed in yet or not anonymously -> sign in
+        // If user is not signed in yet or not anonymously ->
+        // allow user to sign in anonymously or with Google
         if (firebaseAuth.currentUser == null || firebaseAuth.currentUser?.isAnonymous == false) {
             button_login_activity_skip.setOnClickListener {
                 skipSignIn()
             }
+            button_login_activity_google.setOnClickListener {
+                signInWithGoogle()
+            }
         }
-        // If user signed in anonymously -> make button invisible
+        // If user signed in anonymously ->
+        // make button for anonymous sign in invisible
+        // and convert an anonymous account to a permanent account
         else {
             button_login_activity_skip.visibility = View.INVISIBLE
+            button_login_activity_google.setOnClickListener {
+                convertAnonymousAccountToPermanent()
+            }
         }
     }
-
 
     private fun signInWithGoogle() {
         Log.w("Login Activity", "Google Sign In")
@@ -59,7 +63,7 @@ class LoginActivity : AppCompatActivity() {
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
-    private fun skipSignIn(){
+    private fun skipSignIn() {
         Log.w("Login Activity", "Skip Sign In")
         firebaseAuth.signInAnonymously()
             .addOnCompleteListener(this) { task ->
@@ -74,16 +78,36 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
+    private fun convertAnonymousAccountToPermanent() {
+        Log.w("Login Activity", "Convert Anonymous account to permanent")
+        val convertIntent = googleSignInClient.signInIntent
+        startActivityForResult(convertIntent, RC_CONVERT_ACC)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        // Request for Google sign in
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)
                 firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("Login Activity", "Google sign in failed", e)
+            }
+        }
+
+        // Request for convert Anonymous account to permanent
+        if (requestCode == RC_CONVERT_ACC) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseConvertAnonymousAccountToPermanentGoogle(account!!)
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
                 Log.w("Login Activity", "Google sign in failed", e)
@@ -101,12 +125,27 @@ class LoginActivity : AppCompatActivity() {
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d("Login Activity", "signInWithCredential:success")
-                    val user = firebaseAuth.currentUser
                     // Start Main Activity
                     startActivity(Intent(this, MainActivity::class.java))
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w("Login Activity", "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
+    private fun firebaseConvertAnonymousAccountToPermanentGoogle(googleSignInAccount: GoogleSignInAccount) {
+        Log.d("Login Activity", "firebaseConvertToGoogle:" + googleSignInAccount.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(googleSignInAccount.idToken, null)
+        firebaseAuth.currentUser?.linkWithCredential(credential)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("Login Activity", "linkWithCredential:success")
+                    // Start Main Activity
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Log.w("Login Activity", "linkWithCredential:failure", task.exception)
                 }
             }
     }
