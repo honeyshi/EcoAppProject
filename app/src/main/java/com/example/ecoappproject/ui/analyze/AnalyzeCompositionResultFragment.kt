677b05fa8c00143ff88e9bc6a061ccf62d9bfaa1
Katerina Tikhonova <missoctober390@gmail.com>
2020-05-05 20:00:00 +0300

    Custom rating bar for ingredients analysis; analysis fragment refactoring

--- app/src/main/java/com/example/ecoappproject/ui/analyze/AnalyzeCompositionResultFragment.kt
+++ app/src/main/java/com/example/ecoappproject/ui/analyze/AnalyzeCompositionResultFragment.kt
@@ -6,23 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ecoappproject.R
 import com.example.ecoappproject.adapter.CompositionAnalysisAdapter
 
 class AnalyzeCompositionResultFragment : Fragment() {
-    private lateinit var analyzeViewModel: AnalyzeViewModel
+    private val analyzeViewModel: AnalyzeViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        analyzeViewModel =
-            ViewModelProviders.of(requireActivity()).get(AnalyzeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_analyze_whole, container, false)
         val textViewApprovalMessage =
             root.findViewById<TextView>(R.id.text_view_whole_analysis_result_approval_message)
@@ -32,27 +30,31 @@ class AnalyzeCompositionResultFragment : Fragment() {
             root.findViewById<TextView>(R.id.text_view_whole_analysis_result_not_found_list)
         val recyclerViewAnalyzeWhole =
             root.findViewById<RecyclerView>(R.id.recycler_view_analyze_whole)
-        recyclerViewAnalyzeWhole.layoutManager = LinearLayoutManager(activity!!.applicationContext)
+        recyclerViewAnalyzeWhole.layoutManager =
+            LinearLayoutManager(requireActivity().applicationContext)
 
-        analyzeViewModel.getIsApproved().observe(this, Observer {
+        analyzeViewModel.getIsApproved().observe(viewLifecycleOwner, Observer {
             // approved
-            if (it) textViewApprovalMessage.text = resources.getString(R.string.text_view_whole_analysis_result_approved_message)
+            if (it) textViewApprovalMessage.text =
+                resources.getString(R.string.text_view_whole_analysis_result_approved_message)
             // not approved
-            else textViewApprovalMessage.text = resources.getString(R.string.text_view_whole_analysis_result_not_approved_message)
+            else textViewApprovalMessage.text =
+                resources.getString(R.string.text_view_whole_analysis_result_not_approved_message)
         })
 
-        analyzeViewModel.getIsNotFound().observe(this, Observer {
+        analyzeViewModel.getIsNotFound().observe(viewLifecycleOwner, Observer {
             // not found
             if (it) {
                 textViewNotFoundMessage.visibility = View.VISIBLE
-                analyzeViewModel.getNotFoundIngredients().observe(this, Observer { notFoundList ->
-                    textViewNotFoundList.text = notFoundList
-                    textViewNotFoundList.visibility = View.VISIBLE
-                })
+                analyzeViewModel.getNotFoundIngredients()
+                    .observe(viewLifecycleOwner, Observer { notFoundList ->
+                        textViewNotFoundList.text = notFoundList
+                        textViewNotFoundList.visibility = View.VISIBLE
+                    })
             }
         })
 
-        analyzeViewModel.getIngredientItemList().observe(this, Observer{
+        analyzeViewModel.getIngredientItemList().observe(viewLifecycleOwner, Observer {
             val compositionAnalysisAdapter = CompositionAnalysisAdapter(it)
             recyclerViewAnalyzeWhole.adapter = compositionAnalysisAdapter
         })
