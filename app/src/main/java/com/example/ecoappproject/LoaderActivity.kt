8841694f1dd4d3df18d97a398ce28140d6819135
Katerina Tikhonova <missoctober390@gmail.com>
2020-05-07 22:47:47 +0300

    Calculate current day for challenge in Loader activity

--- app/src/main/java/com/example/ecoappproject/LoaderActivity.kt
+++ app/src/main/java/com/example/ecoappproject/LoaderActivity.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.example.ecoappproject.objects.ChallengeObject
 import kotlinx.android.synthetic.main.activity_loader.*
 
 
@@ -14,9 +15,13 @@ class LoaderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loader)
-        
+
         Glide.with(this).asGif().load(R.drawable.ic_loader_activity)
             .into(image_view_loader_activity_icon)
+
+        Log.w(LOADER_ACTIVITY_TAG, "Start increasing current day for challenges")
+        ChallengeObject.increaseCurrentDayForStartedChallenges()
+
         Handler().postDelayed(startMainActivity, 1000)
     }
 
--- app/src/main/java/com/example/ecoappproject/interfaces/OnGetStartedChallengePresenceListener.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ecoappproject.interfaces
-
-interface OnGetStartedChallengePresenceListener {
-    fun OnGetStartedChallengePresence(isPresented: Boolean)
-}
